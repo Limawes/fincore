1e00d446943861c0f17b99fe2bf8f4e4b7e29294
Wesley Lima <wesleyaz0909@gmail.com>
2026-08-31 18:51:51 -0300

    First commit

---
+++ src/test/java/br/com/fincore/FincoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fincore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FincoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
