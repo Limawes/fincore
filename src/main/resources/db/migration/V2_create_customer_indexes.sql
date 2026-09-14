dba90a10d03c90b4f253b3de14b4438578586211
Wesley Lima <wesleyaz0909@gmail.com>
2026-09-14 14:07:55 -0300

    feat: adjust table name from migration

--- src/main/resources/db/migration/V2_create_customer_indexes.sql
+++ src/main/resources/db/migration/V2_create_customer_indexes.sql
@@ -1,3 +1,3 @@
-CREATE INDEX idx_customer_email ON customer (email);
-CREATE INDEX idx_customer_status ON customer (status);
-CREATE INDEX idx_customer_created_at ON customer (created_at);
+CREATE INDEX idx_customer_email ON customers (email);
+CREATE INDEX idx_customer_status ON customers (status);
+CREATE INDEX idx_customer_created_at ON customers (created_at);
